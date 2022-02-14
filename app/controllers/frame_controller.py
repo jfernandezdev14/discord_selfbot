2bb92212e418c0f83bf42d0a4b891b5da530b51e
Juan Sebastian Fernandez <jfernandezdev14@gmail.com>
2022-02-14 12:03:19 -0500

    Adding initial configuration of the bot manager tab

--- app/controllers/frame_controller.py
+++ app/controllers/frame_controller.py
@@ -1,5 +1,6 @@
 import json
-from tkinter import Label, Button, Toplevel, messagebox, Canvas, Entry, W, E
+from tkinter import Label, Button, Toplevel, messagebox, Canvas, Entry, W, E, Listbox, Scrollbar
+from tkinter.constants import RIGHT, BOTH
 
 from app.components.frame_container import FrameContainer
 from app.components.menu_bar import MenuBar
@@ -15,16 +16,71 @@ class FrameController(object):
         self.bot_token = None
         self.bot_proxy = None
         self.server_id = None
+        self.bot_manager_tab = None
+        self.bot_configuration_tab = None
+        self.manager_tab_bot_list = None
+        self.manager_tab_active_bot_list = None
 
     def build_frame(self):
-        self.root.title("Tab Widget")
+        self.root.title("Discord SelfBot Manager")
         self.root.geometry("600x400")
         self.setup_menu_bar()
         tab_manager = TabManager(self.root)
-        tab_1 = FrameContainer(tab_manager)
-        tab_1.include_components()
-        tab_manager.add_tab(tab_1, 'Primer Tab')
+        self.bot_manager_tab = FrameContainer(tab_manager)
+        tab_manager.add_tab(self.bot_manager_tab, 'Bot Manager')
         tab_manager.pack()
+        self.setup_bot_manager_tab()
+        self.bot_configuration_tab = FrameContainer(tab_manager)
+        self.bot_configuration_tab.include_components()
+        tab_manager.add_tab(self.bot_configuration_tab, 'Bot Configuration')
+        tab_manager.pack()
+
+    def setup_bot_manager_tab(self):
+
+        # Define Registered bot list
+        Label(master=self.bot_manager_tab, text='Registered Bots').grid(row=0, column=0)
+        Label(master=self.bot_manager_tab, text='Active Bots').grid(row=0, column=6)
+        self.manager_tab_bot_list = Listbox(self.bot_manager_tab)
+        scrollbar_manager_bot_list = Scrollbar(self.bot_manager_tab)
+        scrollbar_manager_bot_list.grid(row=1, column=1, rowspan=6, sticky='w')
+        self.manager_tab_bot_list.config(yscrollcommand=scrollbar_manager_bot_list.set)
+        scrollbar_manager_bot_list.config(command=self.manager_tab_bot_list.yview)
+
+        try:
+            bot_configuration_file = open('./app/data/bots_configuration.json', 'r')
+            bot_configuration = json.load(bot_configuration_file)
+            bot_configuration_file.close()
+            if 'accounts' in bot_configuration:
+                i = 1
+                for account in bot_configuration['accounts']:
+                    self.manager_tab_bot_list.insert(i, account['name'])
+                    i = i + 1
+
+        except Exception as e:
+            print("Bot configuration file is corrupted or doesn't exist. Failed loading existing bots" + e)
+        self.manager_tab_bot_list.grid(row=1, rowspan=6, column=0, sticky='nws')
+
+        # Define buttons to activate the bots
+        activate_bot_button = Button(master=self.bot_manager_tab, text='Activate Bot >>', command=None)
+        activate_bot_button.grid(row=2, column=2)
+        activate_all_bots_button = Button(master=self.bot_manager_tab, text='Activate all >>', command=None)
+        activate_all_bots_button.grid(row=3, column=2)
+        deactivate_bot_button = Button(master=self.bot_manager_tab, text='<< Deactivate Bot', command=None)
+        deactivate_bot_button.grid(row=4, column=2)
+        deactivate_all_bots_button = Button(master=self.bot_manager_tab, text='<< Deactivate all', command=None)
+        deactivate_all_bots_button.grid(row=5, column=2)
+
+        # Define active bots list
+        self.manager_tab_active_bot_list = Listbox(self.bot_manager_tab)
+        self.manager_tab_active_bot_list.grid(row=1, rowspan=6, column=6, sticky='nes')
+
+        self.bot_manager_tab.columnconfigure(1, weight=1)
+        self.bot_manager_tab.rowconfigure(1, weight=1)
+        self.bot_manager_tab.rowconfigure(2, weight=1)
+        self.bot_manager_tab.rowconfigure(3, weight=1)
+        self.bot_manager_tab.rowconfigure(4, weight=1)
+        self.bot_manager_tab.rowconfigure(5, weight=1)
+        self.bot_manager_tab.rowconfigure(6, weight=1)
 
     def setup_menu_bar(self):
         menu_bar = MenuBar(self.root, 'File')
@@ -53,7 +109,7 @@ class FrameController(object):
         self.bot_proxy = bot_proxy
         Label(master=self.sub_window, text='Enter Discord Server ID').grid(row=3)
         server_id = Entry(master=self.sub_window)
-        server_id .grid(row=3, column=1, sticky='ew')
+        server_id.grid(row=3, column=1, sticky='ew')
         self.server_id = server_id
         Button(self.sub_window, text='Connect Bot',
                command=self.add_bot_configuration).grid(row=4, columnspan=2)
@@ -73,13 +129,14 @@ class FrameController(object):
                     raise
 
             new_bot_configuration = {
-                "name": self.bot_name,
-                "token": self.bot_token,
-                "proxy": self.bot_proxy,
+                "name": self.bot_name.get(),
+                "token": self.bot_token.get(),
+                "proxy": self.bot_proxy.get(),
                 "user_id": None,
                 "channelIDs_time": None,
                 "file_contaning_message": None
             }
+            bot_configuration['accounts'].append(new_bot_configuration)
 
             bot_configuration_file = open('./app/data/bots_configuration.json', 'w')
             json.dump(bot_configuration, bot_configuration_file)
